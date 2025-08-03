b72cfecca24495e5fef7da518706f185aee38808
Tanvir Ahmmad <tanvirahmmad33@gmail.com>
2025-08-03 18:50:36 +0600

    forecast page updated

--- js/app.js
+++ js/app.js
@@ -98,8 +98,6 @@ $("#weather-form").on("submit", function (e) {
     url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
     method: "GET",
     success: function (data) {
-      console.log(data);
-
       // top section
       $("#city-name").text(data.name);
       $("#curent-temperature-amount").text(data.main.temp.toFixed(1));
@@ -220,7 +218,6 @@ $(".fahrenheit-btn").on("click", function () {
 
 function generateForecastDays() {
   const today = new Date();
-  console.log(today);
 
   for (let i = 1; i <= 5; i++) {
     const forecastDate = new Date(today);
@@ -233,8 +230,142 @@ function generateForecastDays() {
     });
     const optionValue = i;
 
-    $("#forecast-day").append(`<option value="${optionValue}">${optionText}</option>`);
+    $("#forecast-day").append(`<option value="${optionValue}" class="option-size">${optionText}</option>`);
   }
 }
 
 generateForecastDays();
+
+// ******************************************************************************
+// *************** Function to generate dates for the next 5 days ***************
+// ******************************************************************************
+
+$("#forecast-form").on("submit", function (e) {
+  e.preventDefault();
+
+  const city = $("#search-city-box").val();
+  let forecastDayIdx = $("#forecast-day").val();
+
+  if (city == "" || forecastDayIdx == "") {
+    let errorText = "";
+
+    if (city == "" && forecastDayIdx == "") {
+      errorText = "City name and forecast day are required!";
+    } else if (city == "") {
+      errorText = "City name is required!";
+    } else if (forecastDayIdx == "") {
+      errorText = "Forecast day is required!";
+    }
+
+    Swal.fire({
+      title: errorText,
+      toast: true,
+      position: "top-right",
+      showConfirmButton: true,
+      confirmButtonText: "OK",
+      confirmButtonColor: "#3085d6",
+      timer: 5000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+      customClass: {
+        confirmButton: "swal2-confirm",
+      },
+    });
+
+    return;
+  }
+
+  $("#forecat-search-box").addClass("d-none");
+  $(".forecast-boxes").css({
+    "background-image": "none",
+    "padding-top": "0",
+  });
+  $("#forecat-search-result-box").removeClass("d-none");
+
+  forecastDayIdx = Number(forecastDayIdx);
+
+  const today = new Date();
+  const forecast = new Date(today);
+  forecast.setDate(today.getDate() + 1);
+  const forecastCheck = forecast.toISOString().split("T")[0];
+  const forecastDate =
+    forecast.toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "long",
+    }) +
+    ", " +
+    forecast.toLocaleDateString("en-GB", {
+      weekday: "long",
+    });
+  $("#forecast-date").text(forecastDate);
+
+  const apiKey = "ba7455a48b9d0b5a432e6d0a752a56fc";
+  console.log(forecastDate);
+
+  $.ajax({
+    url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`,
+    method: "GET",
+    success: function (data) {
+      let idx = 0;
+
+      for (let i = 0; i < 40; i++) {
+        let forecast = data.list[i];
+
+        const forecastDateFromAPI = forecast.dt_txt.split(" ")[0];
+        if (forecastCheck === forecastDateFromAPI) {
+          console.log(forecast);
+
+          const time = new Date(forecast.dt_txt).toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", hour12: true });
+          const feelsLike = forecast.main.feels_like.toFixed(1) + " °C";
+          const condition = forecast.weather[0].description;
+          const minTemperature = forecast.main.temp_min.toFixed(1) + " °C";
+          const maxTemperature = forecast.main.temp_max.toFixed(1) + " °C";
+          const humidity = forecast.main.humidity + "%";
+          const cloudCoverage = forecast.clouds.all + "%";
+          const airPressure = forecast.main.pressure + " hPa";
+          const visibility = (forecast.visibility / 1000).toFixed(2) + " km";
+          const windSpeed = (forecast.wind.speed * 3.6).toFixed(2) + " km/h";
+          const degree = forecast.wind.deg;
+          const windDegree = (degree) => {
+            if (degree >= 0 && degree <= 22.5) return "North";
+            else if (degree > 22.5 && degree <= 67.5) return "North-East";
+            else if (degree > 67.5 && degree <= 112.5) return "East";
+            else if (degree > 112.5 && degree <= 157.5) return "South-East";
+            else if (degree > 157.5 && degree <= 202.5) return "South";
+            else if (degree > 202.5 && degree <= 247.5) return "South-West";
+            else if (degree > 247.5 && degree <= 292.5) return "West";
+            else if (degree > 292.5 && degree <= 337.5) return "North-West";
+            return "North";
+          };
+
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-title-${idx}`).text(time);
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-feels-like-${idx}`).text(feelsLike);
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-codition-${idx}`).text(condition);
+          $(`#forecast-describe-box-${idx}`)
+            .find(`#forecast-describe-box-temperature-${idx}`)
+            .text(minTemperature + " - " + maxTemperature);
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-humidity-amount-${idx}`).text(humidity);
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-cloud-coverage-amount-${idx}`).text(cloudCoverage);
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-air-pressure-amount-${idx}`).text(airPressure);
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-visibility-amount-${idx}`).text(visibility);
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-wind-speed-amount-${idx}`).text(windSpeed);
+          $(`#forecast-describe-box-${idx}`).find(`#forecast-describe-box-wind-degree-amount-${idx}`).text(windDegree);
+
+          idx++;
+        }
+      }
+    },
+    error: function (xhr, status, error) {
+      if (xhr.status === 404) {
+        alert("City not found. Please try again.");
+      } else if (xhr.status === 401) {
+        alert("Invalid API key. Please check your API key.");
+      } else {
+        alert("There was an error fetching the data: " + error);
+      }
+    },
+  });
+});
